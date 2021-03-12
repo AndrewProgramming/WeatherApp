65831eb6e6a842290f71d35509008c75ff2872c5
Andrew <kobe73er@gmail.com>
2021-03-12 11:09:54 +0800

    add executing permission for the script

--- scripts/stop-app.sh
+++ scripts/stop-app.sh
@@ -2,6 +2,7 @@ NODE_PORT=$(lsof -t -i:3001)
 if [ -z "${NODE_PORT}" ]; then
     echo "no node app running..."
   else
+    echo "stop app running on port ${NODE_PORT}"
     kill -9 ${NODE_PORT}
 fi
 
